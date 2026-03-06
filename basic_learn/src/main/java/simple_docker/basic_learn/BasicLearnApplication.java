62db0152010ece8febbd5bab996add4b21ae3ae9
Aakib Khan <mohdaakibkhan35@gmail.com>
2026-03-06 16:12:39 +0530

    this is only for learnig docker container

---
+++ basic_learn/src/main/java/simple_docker/basic_learn/BasicLearnApplication.java
@@ -0,0 +1,13 @@
+package simple_docker.basic_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicLearnApplication.class, args);
+	}
+
+}
---
+++ basic_learn/src/test/java/simple_docker/basic_learn/BasicLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package simple_docker.basic_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
