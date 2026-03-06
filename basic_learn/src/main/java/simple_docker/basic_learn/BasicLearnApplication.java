cf458f9144a7f4e8f4ba2186cee86324eb1fd88d
Aakib Khan <mohdaakibkhan35@gmail.com>
2026-03-06 16:25:08 +0530

    basic date

--- basic_learn/src/main/java/simple_docker/basic_learn/BasicLearnApplication.java
+++ basic_learn/src/main/java/simple_docker/basic_learn/BasicLearnApplication.java
@@ -10,4 +10,5 @@ public class BasicLearnApplication {
 		SpringApplication.run(BasicLearnApplication.class, args);
 	}
 
+
 }
--- basic_learn/src/main/java/simple_docker/basic_learn/basic.java
+++ basic_learn/src/main/java/simple_docker/basic_learn/basic.java
@@ -1,4 +1,10 @@
 package simple_docker.basic_learn;
+import java.util.Date;
 
 public class basic {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println("hello world"+"docker current date"+currentDate);
+
+    }
 }
